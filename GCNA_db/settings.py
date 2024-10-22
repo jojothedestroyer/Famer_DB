379428188a489c1f489ae0537053f3aa2f47505c
jojothedestroyer <107715395+jojothedestroyer@users.noreply.github.com>
2024-10-22 14:50:34 -0400

    Update settings.py /static/

--- GCNA_db/settings.py
+++ GCNA_db/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js','serviceworker.js')
